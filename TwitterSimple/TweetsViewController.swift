dbfa5c103b1ccc6316e11bee68b3bd57b9633871
Matt Carroll <matt.carroll@rd.io>
2015-05-04 21:01:41 -0700

    Deselect row after selecting

--- TwitterSimple/TweetsViewController.swift
+++ TwitterSimple/TweetsViewController.swift
@@ -90,4 +90,8 @@ class TweetsViewController: UIViewController, UITableViewDataSource, UITableView
             }
         }
     }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
 }
